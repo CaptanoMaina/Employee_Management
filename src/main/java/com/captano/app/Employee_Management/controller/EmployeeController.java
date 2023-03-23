b07b684e681fe996f53ebc1a540db33e918edd0f
Captano Maina <captanomaina@gmail.com>
2023-03-23 21:43:28 +0400

    Built a REST API to retrieve all employee records

---
+++ src/main/java/com/captano/app/Employee_Management/controller/EmployeeController.java
@@ -0,0 +1,25 @@
+package com.captano.app.Employee_Management.controller;
+
+import com.captano.app.Employee_Management.model.EmployeeModel;
+import com.captano.app.Employee_Management.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/")
+public class EmployeeController {
+
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    @GetMapping("/employees")
+    public List<EmployeeModel> getAllEmployees(){
+        return employeeRepository.findAll();
+    }
+
+
+}
