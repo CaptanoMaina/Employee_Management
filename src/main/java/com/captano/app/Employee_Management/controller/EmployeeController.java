78ed2a69830f5eb5dd5541ac3b61a704b00c16c4
Captano Maina <captanomaina@gmail.com>
2023-03-24 21:51:14 +0400

    REST api to create employee record

--- src/main/java/com/captano/app/Employee_Management/controller/EmployeeController.java
+++ src/main/java/com/captano/app/Employee_Management/controller/EmployeeController.java
@@ -3,10 +3,7 @@ package com.captano.app.Employee_Management.controller;
 import com.captano.app.Employee_Management.model.EmployeeModel;
 import com.captano.app.Employee_Management.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -21,6 +18,11 @@ public class EmployeeController {
     @GetMapping("/employees")
     public List<EmployeeModel> getAllEmployees(){
         return employeeRepository.findAll();
+    }
+
+    @PostMapping("/employees")
+    public EmployeeModel createEmployee(@RequestBody EmployeeModel employee){
+        return employeeRepository.save(employee);
     }
 
 
--- src/main/java/com/captano/app/Employee_Management/model/EmployeeModel.java
+++ src/main/java/com/captano/app/Employee_Management/model/EmployeeModel.java
@@ -1,6 +1,7 @@
 package com.captano.app.Employee_Management.model;
 
 import jakarta.persistence.*;
+import org.hibernate.annotations.CreationTimestamp;
 import org.springframework.data.annotation.CreatedDate;
 
 import java.util.Date;
@@ -26,7 +27,7 @@ public class EmployeeModel {
     @Column(name="region_name")
     private String region_name;
 
-    @CreatedDate
+    @CreationTimestamp
     private Date created_date;
 
     public EmployeeModel(){}
