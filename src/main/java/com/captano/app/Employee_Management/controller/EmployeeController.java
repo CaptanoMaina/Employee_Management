4ea88ed7c9013a4bde0febedc86337b90d51c3ed
Captano Maina <captanomaina@gmail.com>
2023-03-24 18:52:00 +0400

    fixed CORS error

--- src/main/java/com/captano/app/Employee_Management/controller/EmployeeController.java
+++ src/main/java/com/captano/app/Employee_Management/controller/EmployeeController.java
@@ -3,12 +3,14 @@ package com.captano.app.Employee_Management.controller;
 import com.captano.app.Employee_Management.model.EmployeeModel;
 import com.captano.app.Employee_Management.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/api/v1/")
 public class EmployeeController {
